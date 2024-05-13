ae94adeed1fa3eb224ce1e960faccf51336c91ef
Farhad <farhadmikky@gmail.com>
2024-05-13 10:32:11 +0600

    practice rest api

---
+++ api_fetch_all.php
@@ -0,0 +1,27 @@
+<?php 
+
+include "config.php";
+
+header('Content-Type: application/json');
+header('Acess-Control-Allow-Origin: *');
+
+
+$sql = "SELECT * FROM students";
+$result = mysqli_query($conn, $sql) or die("SQL Query Failed.");
+ 
+if (mysqli_num_rows($result) > 0) {
+	
+	$output = mysqli_fetch_all($result, MYSQLI_ASSOC);
+	echo json_encode($output);
+}else{
+
+	echo json_encode(array('message' => 'No Record Found.', 'status' => false));
+}
+
+
+
+
+
+
+
+ ?>
